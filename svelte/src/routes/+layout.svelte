1e183cdb04ebd2ff0ebf7b55fed2dc31f5e8d468
Aaron <aarontaylorg33k@gmail.com>
2024-09-09 16:03:49 -0400

    clean up

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import { onMount } from 'svelte';
 	import Header from '../components/header.svelte';
 	import '../tailwind.css';
-  import {closeWebSocket} from '../lib/websocket';
+	import { closeWebSocket } from '../lib/websocket';
 
 	onMount(() => {
 		return () => {
@@ -20,4 +20,3 @@
 <main class="flex flex-col">
 	<slot />
 </main>
-
