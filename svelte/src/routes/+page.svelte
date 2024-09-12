7d995291f9c745b9127291fea83e6a8204ff677f
Aaron <aarontaylorg33k@gmail.com>
2024-09-12 10:42:38 -0400

    latest update

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -5,7 +5,7 @@
 <!--- the tables are controllable by passing them an array of the groupin_lists you want to display-->
 
 <div class="container mx-auto my-4 mt-12">
-<h1 class="text-gray-800">Master View</h1>
+<h1 class="text-gray-800">Invoice View</h1>
 <p>Tables are bound to routes for improved UX of being able to bookmark and share datasets</p>
 <div class="flex flex-row ">
 <div class=" h-[50vh]  overflow-auto border-gray-200 border ">
@@ -15,15 +15,20 @@
 </div>
 
 <div class="container mx-auto my-4 mb-12">
-<h1>Reusable</h1>
-<p>Just set groupingLevels to your DuckDB grouping levels, ex. 0, 2</p>
+<h1>Reusable & Connected</h1>
+<p>Just set groupingLevels to your DuckDB grouping levels, ex. 0, 2. All tables listen to the same metrics store</p>
 <div class="flex flex-row gap-4">
-	<div class="w-1/2  h-[40vh] overflow-auto border border-gray-200 ">
+	
+	<div class="w-1/3 h-[40vh] overflow-auto border border-gray-200">
+		<LiquidTables groupingLevels={[0, 1]} />
+	</div>
+	<div class="w-1/3  h-[40vh] overflow-auto border border-gray-200 ">
 		<LiquidTables groupingLevels={[0, 2]} />
 	</div>
-	<div class="w-1/2 h-[40vh] overflow-auto border border-gray-200">
+	<div class="w-1/3 h-[40vh] overflow-auto border border-gray-200">
 		<LiquidTables groupingLevels={[0, 3]} />
 	</div>
+	
 </div>
 </div>
 
